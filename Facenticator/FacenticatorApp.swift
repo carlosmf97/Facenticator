e7aa15423f829db8d22348491996570e6daaba19
carlosmf97 <carlos.cmf97@gmail.com>
2025-01-01 20:51:52 +0100

    Initial Commit

---
+++ Facenticator/FacenticatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacenticatorApp.swift
+//  Facenticator
+//
+//  Created by Carlos Muñoz Fernández on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct FacenticatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
